afd984498b00bf2730640027f69bfb30150728eb
taila.santos <tailasantos41@gmail.com>
2022-04-13 14:18:29 -0300

    Splash and Login Working design done

--- app/src/main/java/com/example/marvelheroes/screens/login/ui/LoginActivity.kt
+++ app/src/main/java/com/example/marvelheroes/screens/login/ui/LoginActivity.kt
@@ -2,14 +2,15 @@ package com.example.marvelheroes.screens.login.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.marvelheroes.databinding.ActivitySplashBinding
+import com.example.marvelheroes.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/marvelheroes/screens/splash/ui/SplashActivity.kt
+++ app/src/main/java/com/example/marvelheroes/screens/splash/ui/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.example.marvelheroes.screens.splash.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.marvelheroes.databinding.ActivitySplashBinding
+import com.example.marvelheroes.screens.login.ui.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
 
@@ -10,6 +13,11 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        },200)
     }
 }
