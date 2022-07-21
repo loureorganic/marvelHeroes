145acec38b2a936cc6ae7114c827c3226ff4a783
taila.santos <tailasantos41@gmail.com>
2022-07-21 09:21:43 -0300

    [GRADLE] - Hiding API Keys and important archives

--- app/src/main/java/com/example/marvelheroes/repositories/network/api/utils/ApiConstants.kt
+++ app/src/main/java/com/example/marvelheroes/repositories/network/api/utils/ApiConstants.kt
@@ -3,14 +3,14 @@ package com.example.marvelheroes.repositories.network.api.utils
 import com.example.marvelheroes.BuildConfig
 import java.math.BigInteger
 import java.security.MessageDigest
-import java.security.Timestamp
 import java.time.Instant
 import java.time.format.DateTimeFormatter
 
 object ApiConstants {
-    const val BASE_URL = "https://gateway.marvel.com"
+    const val BASE_URL = BuildConfig.BASE_URL
     const val PRIVATE_KEY = BuildConfig.API_KEY_PRIVATE
     const val PUBLIC_KEY = BuildConfig.API_KEY_PUBLIC
+
 
     fun generateTimestamp(): String {
         return DateTimeFormatter.ISO_INSTANT.format(Instant.now())
