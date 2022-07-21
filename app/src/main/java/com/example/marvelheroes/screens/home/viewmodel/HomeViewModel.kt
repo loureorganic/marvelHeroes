3d39d2504a25105805db3f6569f1f21830bf848c
loureorganic <tailasantos41@gmail.com>
2022-07-21 09:57:22 -0300

    [REFACTORING] - Removing non used extensions

--- app/src/main/java/com/example/marvelheroes/screens/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/marvelheroes/screens/home/viewmodel/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.marvelheroes.screens.home.viewmodel
 
-
 import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
@@ -15,8 +14,6 @@ import com.example.marvelheroes.screens.home.services.ServicesHome
 import com.example.marvelheroes.screens.home.ui.utils.SearchWidgetState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
